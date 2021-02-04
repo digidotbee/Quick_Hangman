d69daf181de66e85c65b927553ab825e4b9877a0
Punchcode <punchcode@Punchcodes-MBP.fios-router.home>
2021-02-04 13:12:49 -0500

    updates

--- game.js
+++ game.js
@@ -1,55 +1,69 @@
 import {commonWords} from '/constants'
 
-let wordToGuess = ''
-
-// Filter through the array of common words for words with a length that is >= 3
-
-const wordList = 
-commonWords.filter(word => word.length >= 3) 
-
-// console.log(wordList)
-
-// A word is randomly chosen from the array of commonWords for which the user will guess letters:
-
-// write a function for random math stuffs
-
-function random () {
-    wordToGuess = wordList[Math.floor(Math.random() * wordList.length)]
-    // console.log(wordToGuess)
+console.log(commonWords)
+function makeRandomWord() {
+    const filteredWordList = commonWords.filter(word => word.length >= 3) 
+    const randomIndex=
+        Math.floor(Math.random() * filteredWordList.length)
+    const randomWord = filteredWordList[randomIndex]
+    return randomWord
 }
 
-random ()
-console.log(wordToGuess)
+const dashContainer = document.querySelector('#dashes')
+const btnContainer = document.querySelector('#letters')
+const randomWord = makeRandomWord()
+console.log(randomWord)
 
+function makeDashes(word, guesses) {
+    const wordArray = randomWord.split('').map(letter => {
+        const showLetter = guesses.includes(letter)
+        const letterOrNot = showLetter ? letter : ''
+        console.log(letter, showLetter, letterOrNot, guesses)    
+        return `<div class="dash">${letterOrNot}</div>`
+    })
+    const dashString = wordArray.join('')
+    const dashContainer = document.querySelector('#dashes')
+    dashContainer.innerHTML = dashString    
+}
 // II.
 // User guesses from a list of letters and those letters are compared to the string of words in the wordToGuess:
 
 // A. first .map some letter buttons and style them in main.css
 
+const btns = 'a b c d e f g h i j k l m n o p q r s t u v w x y z' 
 
-const letterBtns = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
-
-const letterArray = letterBtns.split(" ")
+const letterArray = btns.split(" ")
 
 // console.log(letterArray)
 
 const keyboard =
 letterArray.map (function(letter) {
     return `
-    <button class= "letter-btns" id= "btn ${letter}">${letter}</button>`
+    <button class= "letter-btns" id= "btn ${letter}"> ${letter} </button>`
 }).join(' ')
 
-console.log(keyboard)
+// console.log(keyboard)
 
-document.getElementById('#letters').innerHTML = keyboard
+document.getElementById('letters').innerHTML = keyboard
 
 // Why cant I inject these into the DOM without everything breaking?!?!
 
-// Track what letters the user as guessed and set it into the DOM
-// let lettersGuessed = []
-// function letterClickTracking(btn) {
-//     if 
-// }
+// add an eventlistner that logs what letter user has chosen...
+
+function letterClickTracking(e) {
+    
+    const btnTarget = e.target.innerHTML
+    console.log(btnTarget)
+    const letterInsideWord = wordToGuess.includes(btnTarget)
+   console.log(letterInsideWord === btnTarget)
+    
+}
+let letterBtns = document.getElementById('letters')
+letters.addEventListener('click', letterClickTracking)
+
+
+
+
 
 
 
